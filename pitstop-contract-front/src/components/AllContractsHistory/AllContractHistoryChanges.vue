a954d921094c4b77c38bf10344d8fe9869fe245f
Ekaterina Gibova <katyagibova@bk.ru>
2021-05-07 21:01:36 +0300

    added a history of changes to all contracts

---
+++ pitstop-contract-front/src/components/AllContractsHistory/AllContractHistoryChanges.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-data-table
+    disable-pagination
+    :headers="historyTableHeaders"
+    :items="actionsWithUniqueKey"
+    item-key="key"
+    hide-default-footer
+    disable-sort
+    class="order-products"
+    dark
+  >
+  </v-data-table>
+</template>
+
+<script>
+export default {
+    name: 'AllContractsHistoryChanges',
+    props: ["actions"],
+  data() {
+    return {
+      historyTableHeaders: [
+        {
+          text: "",
+          value: "",
+          align: "center"
+        },
+        {
+          text: "Поле",
+          value: "poleName",
+          align: "center"
+        },
+        {
+          text: "Изначальное значение",
+          value: "oldValue",
+          align: "center"
+        },
+        {
+          text: "Новое значение",
+          value: "newValue",
+          align: "center"
+        },
+      ],
+    };
+  },
+  computed: {
+    actionsWithUniqueKey() {
+      return this.actions.map((item, i) => {
+        return { ...item, key: `${item.id}-${i}` };
+      });
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.order-products {
+  .v-data-table__wrapper {
+    background: #5374FF;
+    color:white;
+  }
+}
+</style>
--- pitstop-contract-front/src/components/AllContractsHistory/index.vue
+++ pitstop-contract-front/src/components/AllContractsHistory/index.vue
@@ -5,13 +5,23 @@
             <h2>История изменений</h2>
           </v-col>
       </v-row>
-        <!-- <v-data-table
+        <v-data-table
         :headers="historyTableHeaders"
-        :items="contractHistory"
+        :items="allContractsHistory"
         item-key="date"
         :loading="isLoad"
         show-expand
-        single-expand > -->
+        single-expand>
+          <template 
+          v-slot:expanded-item="{ headers, item }">
+            <td class="inside" 
+            :colspan="headers.length">
+              <AllContractsHistoryChanges
+              :actions="item.actions"
+              />
+            </td>
+          </template>
+        </v-data-table>
       <v-row>
         
       </v-row>     
@@ -19,16 +29,67 @@
 </template>
 
 <script>
+import Axios from "axios";
+import { GET_CONTRACT_LOGS } from "@/api";
+import AllContractsHistoryChanges from "./AllContractHistoryChanges"
 
 export default {
   name: 'AllContractsHistory',
   components: {
+    AllContractsHistoryChanges
   },
   data(){
     return{
         allContractsHistory: [],
         isLoad: true,
         isErr: false,
+    }
+  },
+  methods:{
+      async getContractLogs() {
+      try {
+        const { data: allContractsHistory } = await Axios.get(GET_CONTRACT_LOGS);
+        this.allContractsHistory = allContractsHistory;
+      } catch {
+        this.isErr = true;
+      } finally {
+        this.isLoad = false;
+      }
+    },
+  },
+  created(){
+    this.getContractLogs();
+  },
+  computed:{
+    historyTableHeaders(){
+      return[
+        {
+          text: "Номер контракта",
+          value: "",
+          align: "center",
+        },
+        {
+          text: "Дата",
+          value: "date",
+          align: "center",
+        },
+        {
+          text: "Время",
+          value: "time",
+          align: "center",
+        },
+        {
+          text: "Действие",
+          value: "actionType",
+          align: "center",
+        },
+        {
+          text: "Пользователь",
+          value: "userFIO",
+          align: "center",
+        },
+            
+      ]
     }
   },
 }
